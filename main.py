edf02eaa5f9b8ff3e3b8509a410f6495a20dc1a3
Firdavs0636 <65146478+Firdavs0636@users.noreply.github.com>
2023-03-04 22:21:36 +0900

    new

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
+PLACEHOLDER = ['name']
+
 with open("./Input/Names/invited_names.txt", mode="r") as name:
     name_list = name.readlines()
-
-new_name_list = []
-for name in name_list:
-    new_name_list.append(name.strip())
 
 with open("./Input/Letters/starting_letter.txt", mode="r") as example:
     example_letter = example.read()
 
-for edited_name in new_name_list:
-    ready_letter = example_letter.replace('[name]', edited_name)
+    for name in name_list:
+        stripped_name = name.strip()
+        ready_letter = example_letter.replace(PLACEHOLDER, stripped_name)
 
-    with open(f"./Output/ReadyToSend/letter_for_{edited_name}.txt", mode="w") as letter:
-        letter.write(ready_letter)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as letter:
+            letter.write(ready_letter)
